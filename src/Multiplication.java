b2fe7033050b4acfd9d97aec58907ae14bdf1a29
mariuszbyahoo <43074935+mariuszbyahoo@users.noreply.github.com>
2018-11-23 11:51:21 +0100

    Multiplication
    Dodanie funkcji mnożenia wielu liczb

--- src/Multiplication.java
+++ src/Multiplication.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 class Multiplication {
     public void multi(){
         Scanner input = new Scanner(System.in);
-        System.out.println("Podaj czynnik a: ");
-        double factorA = input.nextDouble();
-        System.out.println("Podaj czynnik b: ");
-        double factorB = input.nextDouble();
-        System.out.print("Wynik mnożenia to: ");
-        System.out.print(factorA*factorB);
+        System.out.println("Ile liczb chcesz mnożyć?");
+        int amount = input.nextInt();
+        double numbers[] = new double [amount];
+        for (int i=0; i<amount; i++) {
+            System.out.println("Podaj element " + i );
+            numbers[i] = input.nextDouble();
+        }
+        double result=1;
+        for (int i=0; i<amount; i++) {
+            result *= numbers[i];
+        }
+
+        System.out.println("wynik mnożenia to: " + result);
     }
 }
