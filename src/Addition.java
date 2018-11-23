8ae40a1634137d73bc1cf7dbce1c1c6fc7207aae
mariuszbyahoo <43074935+mariuszbyahoo@users.noreply.github.com>
2018-11-23 11:40:10 +0100

    Addition
    Dodanie funkcji sumowania wielu liczb

--- src/Addition.java
+++ src/Addition.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 class Addition {
     public void add(){
         Scanner input = new Scanner(System.in);
-        System.out.println("Podaj liczbę a: ");
-        double a = input.nextDouble();
-        System.out.println("Podaj luczbę b: ");
-        double b = input.nextDouble();
-        double result = a + b;
+        System.out.println("Ile liczb chcesz dodać?");
+        int amount = input.nextInt();
+        double numbers[] = new double [amount];
+        for (int i=0; i<amount; i++) {
+            System.out.println("Podaj element " + i );
+            numbers[i] = input.nextDouble();
+        }
+        double result=0;
+        for (int i=0; i<amount; i++) {
+            result += numbers[i];
+        }
+
         System.out.println("wynik dodawania to: " + result);
     }
 }
