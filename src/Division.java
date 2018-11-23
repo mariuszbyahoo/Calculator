32ea5d639c10232d5e675ad3ee5e6e85068c1837
mariuszbyahoo <43074935+mariuszbyahoo@users.noreply.github.com>
2018-11-23 11:55:13 +0100

    Division
    Dodanie funkcji dzielenia wielu liczb

--- src/Division.java
+++ src/Division.java
@@ -3,15 +3,23 @@ import java.util.Scanner;
 class Division {
     public void div(){
         Scanner input = new Scanner(System.in);
-        System.out.println("Podaj dzielną: ");
-        double dividend = input.nextDouble();
-        System.out.println("Podaj dzielnik: ");
-        double divisor = input.nextDouble();
-        if(divisor != 0) {
+        System.out.println("Ile liczb chcesz dzielić?");
+        int amount = input.nextInt();
+        double numbers[] = new double [amount];
+        for (int i=0; i<amount; i++) {
+            System.out.println("Podaj element " + i );
+            numbers[i] = input.nextDouble();
+        }
+        double result=numbers[0]*numbers[0];
+        for (int i=0; i<amount; i++) {
+            result /= numbers[i];
+        }
+        System.out.print("Wynik dzielenia to: "+result);
+        /*if(divisor != 0) {
             System.out.print("Wynik dzielenia to: ");
             System.out.print(dividend / divisor);
         }else{
             System.out.println("Nie można dzielić przez 0!");
-        }
+        }*/
     }
 }
